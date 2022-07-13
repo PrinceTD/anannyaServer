0046c96d5d1401ee9895f2731ad058c2e0ba9272
PrinceTD <prince.talukder001@gmail.com>
2022-07-13 19:31:25 +0600

    added

--- index.js
+++ index.js
@@ -16,22 +16,18 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
+        const database = client.db('anannya');
+        const userCollection = database.collection('users');
 
-        const database = client.db('women');
-        const servicesCollection = database.collection('pant');
+        console.log("database succfully")
 
-        // post api
-        app.post('/pant', async(req, res)=>{
-            const service ={
-                "name": "hello",
-                "price" : "300",
-                "description" : "সিলভার চোকার সেট!🌻সাথে কিউট দুল আছে!🥀নিজের করতে অথবা প্রিয়জন কে খুশি করতে নিয়ে নিতে পারেন সেট টি! বিস্তারিত জানতে আমাদের পেইজে ইনবক্স করুন ছবি সহ❤️ বিঃদ্রঃ অনুমতি ছাড়া পেইজের কোনো ছবি অন্যকোথাও ব্যবহার করা নিষেধ ❌",
-                "img" : "https://scontent.fdac137-1.fna.fbcdn.net/v/t39.30808-6/287512436_743710577061110_5313730503848806276_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=zH8Fx8QNBAIAX_m7coq&_nc_ht=scontent.fdac137-1.fna&oh=00_AT8M-Dk8zUyeOcfnA2ghoMEt4ZFfYyEfI6cxFk2An8gwHg&oe=62C028CE"
-            }
-            const result = await servicesCollection.insertOne(service);
-            console.log(result);
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await userCollection.insertOne(user);
+            console.log(result)
+            res.json(result);
         })
-
     }
     finally {
 
