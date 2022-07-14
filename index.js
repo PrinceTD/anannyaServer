d477e3db4c404babda3ba7be820eeaebd794bd8d
PrinceTD <prince.talukder001@gmail.com>
2022-07-14 14:54:43 +0600

    added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('dotenv').config()
 const port = process.env.PORT || 5000
 
 app.use(cors());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gfnwb7o.mongodb.net/?retryWrites=true&w=majority`;
@@ -16,18 +17,31 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        const database = client.db('anannya');
-        const userCollection = database.collection('users');
-
-        console.log("database succfully")
+        const database = client.db('anannya-fashion');
+        const usersCollection = database.collection("users");
 
 
         app.post('/users', async (req, res) => {
             const user = req.body;
-            const result = await userCollection.insertOne(user);
-            console.log(result)
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        });
+
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true }
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
             res.json(result);
         })
+
+
+        console.log("database succfully")
+
+
+
     }
     finally {
 
